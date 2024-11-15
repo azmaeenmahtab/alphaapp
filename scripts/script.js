7f7422ac0c50244d3c3cda61f3075705dcd50330
azmaeenmahtab <mahtabazmaeen7@gmail.com>
2024-11-15 14:26:10 +0600

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,20 @@
 function play() {
     document.getElementById('first-page').style.display = 'none';
-    document.getElementById('second-page').style.display = 'block';
+
+
+    document.getElementById('second-page').classList.remove('hidden')
+
+
+    // creat a random alphabet 
+    let randomChar = getRandomAlphabet()
+    document.getElementById('showAlphabet').innerText = randomChar;
+
+
+    setBgColor(randomChar.toLowerCase())
+
+
+
+
+
+
 }
---
+++ scripts/utility.js
@@ -0,0 +1,11 @@
+function getRandomAlphabet() {
+    const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetArray = alphabets.split('');
+    const randomIndex = Math.floor(Math.random() * alphabets.length - 1);
+    return alphabetArray[randomIndex];
+}
+
+
+function setBgColor(elementid) {
+    document.getElementById(elementid).classList.add('bg-red-400')
+}
